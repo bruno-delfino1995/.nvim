341d9cdba8747e385e54e469efd5bf1261295a4f
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2022-09-12 19:02:17 -0300

    rely on ansible to install packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,21 +1,3 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system {
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	}
-	print "Installing packer close and reopen Neovim..."
-	vim.cmd("packadd packer.nvim")
-end
-
--- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
@@ -174,15 +156,10 @@ return packer.startup({
 		use "TimUntersberger/neogit"
 
 		-- tree with fruits
-		use 'kyazdani42/nvim-web-devicons'
+		use "kyazdani42/nvim-web-devicons"
 
 		-- what better than a tree to explore?
-		use 'kyazdani42/nvim-tree.lua'
-
-		-- Automatically set up your configuration after cloning packer.nvim
-		if PACKER_BOOTSTRAP then
-			require("packer").sync()
-		end
+		use "kyazdani42/nvim-tree.lua"
 	end,
 	config = { git = { clone_timeout = 120 } }
 })
