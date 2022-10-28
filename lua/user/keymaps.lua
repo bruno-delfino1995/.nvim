36faab2fd86351ad262c3173c603796ac57015cd
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2022-10-27 22:40:05 -0300

    remove old keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,7 +5,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<space>", "<nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -32,28 +32,20 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- Reload config
-keymap("n", "<leader>ev", ":sp $MYVIMRC<CR>", opts)
-keymap("n", "<leader>sv", ":source $MYVIMRC<CR>", opts)
-
 -- Consistency
 keymap("n", "U", "<C-r>", opts) -- make `U` behave as redo
 keymap("n", "Y", "y$", opts) -- make `Y` more consistent when related to `D` and `C`
 keymap("n", "S", "hs", opts) -- make `S` behave like `X`, and keep `s` as `x`
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<cr>", opts)
+keymap("n", "<C-Down>", ":resize -2<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+keymap("n", "<S-l>", ":bnext<cr>", opts)
+keymap("n", "<S-h>", ":bprevious<cr>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -61,28 +53,15 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m .+1<cr>==", opts)
+keymap("v", "<A-k>", ":m .-2<cr>==", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
-
--- Terminal --
--- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
-
-keymap("n", "<leader>ff", "<CMD>lua require('telescope.builtin').find_files({ previewer = false, shorten_path = true })<CR>", opts)
-keymap("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", opts)
-keymap("n", "<leader>fb", "<CMD>Telescope buffers<CR>", opts)
-keymap("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", opts)
+keymap("x", "J", ":move '>+1<cr>gv-gv", opts)
+keymap("x", "K", ":move '<-2<cr>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<cr>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<cr>gv-gv", opts)
 
 -- File browser
 keymap("n", "-", "<cmd>lua require('telescope').extensions.file_browser.file_browser({ path = '%:p:h' })<cr>", opts)
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -3,7 +3,8 @@ if not status_ok then
 	return
 end
 
-local setup = {
+
+which_key.setup({
 	plugins = {
 		marks = true, -- shows a list of your marks on ' and `
 		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -17,7 +18,7 @@ local setup = {
 			operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
 			motions = false, -- adds help for motions
 			text_objects = false, -- help for text objects triggered after entering an operator
-			windows = true, -- default bindings on <c-w>
+			windows = true, -- default bindings on <C-w>
 			nav = true, -- misc bindings to work with windows
 			z = true, -- bindings for folds, spelling and others prefixed with z
 			g = true, -- bindings for prefixed with g
@@ -39,8 +40,8 @@ local setup = {
 		group = "+", -- symbol prepended to a group
 	},
 	popup_mappings = {
-		scroll_down = "<c-d>", -- binding to scroll down inside the popup
-		scroll_up = "<c-u>", -- binding to scroll up inside the popup
+		scroll_down = "<C-d>", -- binding to scroll down inside the popup
+		scroll_up = "<C-u>", -- binding to scroll up inside the popup
 	},
 	window = {
 		border = "rounded", -- none, single, double, shadow
@@ -56,7 +57,7 @@ local setup = {
 		align = "left", -- align columns left, center or right
 	},
 	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+	hidden = { "<silent>", "<cmd>", "<cmd>", "<cr>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 	show_help = true, -- show help message on the command line when the popup is visible
 	triggers = "auto", -- automatically setup triggers
 	-- triggers = {"<leader>"} -- or specify a list manually
@@ -67,7 +68,7 @@ local setup = {
 		i = { "j", "k" },
 		v = { "j", "k" },
 	},
-}
+})
 
 local opts = {
 	mode = "n", -- NORMAL mode
@@ -79,50 +80,121 @@ local opts = {
 }
 
 local mappings = {
-	["/"] = { "<cmd>lua require(\"Comment.api\").toggle_current_linewise()<CR>", "Comment" },
-	["a"] = { "<cmd>Startify<cr>", "Dashboard" },
+	["a"] = {
+		"<cmd>Startify<cr>",
+		"Dashboard",
+	},
 	["b"] = {
-		"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+		"<cmd>lua require('telescope.builtin').buffers()<cr>",
 		"Buffers",
 	},
-	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-	["w"] = { "<cmd>w!<CR>", "Save" },
-	["q"] = { "<cmd>q!<CR>", "Quit" },
-	["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-	["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
+	["-"] = {
+		"<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>",
+		"Explorer",
+	},
+	["w"] = {
+		"<cmd>w!<cr>",
+		"Save",
+	},
+	["q"] = {
+		"<cmd>q!<cr>",
+		"Quit",
+	},
+	["c"] = {
+		"<cmd>Bdelete!<cr>",
+		"Close Buffer",
+	},
+	["h"] = {
+		"<cmd>nohlsearch<cr>",
+		"No Highlight",
+	},
 	["f"] = {
-		"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+		"<cmd>lua require('telescope.builtin').find_files()<cr>",
 		"Find files",
 	},
-	["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-	["P"] = { "<cmd>Telescope projects<cr>", "Projects" },
+	["F"] = {
+		"<cmd>Telescope live_grep<cr>",
+		"Find Text",
+	},
+	["P"] = {
+		"<cmd>Telescope projects<cr>",
+		"Projects",
+	},
 
 	p = {
 		name = "Packer",
-		c = { "<cmd>PackerCompile<cr>", "Compile" },
-		i = { "<cmd>PackerInstall<cr>", "Install" },
-		s = { "<cmd>PackerSync<cr>", "Sync" },
-		S = { "<cmd>PackerStatus<cr>", "Status" },
-		u = { "<cmd>PackerUpdate<cr>", "Update" },
+		c = {
+			"<cmd>PackerCompile<cr>",
+			"Compile",
+		},
+		i = {
+			"<cmd>PackerInstall<cr>",
+			"Install",
+		},
+		s = {
+			"<cmd>PackerSync<cr>",
+			"Sync",
+		},
+		S = {
+			"<cmd>PackerStatus<cr>",
+			"Status",
+		},
+		u = {
+			"<cmd>PackerUpdate<cr>",
+			"Update",
+		},
 	},
 
 	g = {
 		name = "Git",
-		g = { "<cmd>lua require 'neogit'.open()<cr>", "Neogit"},
-		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+		g = {
+			"<cmd>lua require 'neogit'.open()<cr>",
+			"Neogit",
+		},
+		j = {
+			"<cmd>lua require 'gitsigns'.next_hunk()<cr>",
+			"Next Hunk",
+		},
+		k = {
+			"<cmd>lua require 'gitsigns'.prev_hunk()<cr>",
+			"Prev Hunk",
+		},
+		l = {
+			"<cmd>lua require 'gitsigns'.blame_line()<cr>",
+			"Blame",
+		},
+		p = {
+			"<cmd>lua require 'gitsigns'.preview_hunk()<cr>",
+			"Preview Hunk",
+		},
+		r = {
+			"<cmd>lua require 'gitsigns'.reset_hunk()<cr>",
+			"Reset Hunk",
+		},
+		R = {
+			"<cmd>lua require 'gitsigns'.reset_buffer()<cr>",
+			"Reset Buffer",
+		},
+		s = {
+			"<cmd>lua require 'gitsigns'.stage_hunk()<cr>",
+			"Stage Hunk",
+		},
 		u = {
 			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
 			"Undo Stage Hunk",
 		},
-		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+		o = {
+			"<cmd>Telescope git_status<cr>",
+			"Open changed file",
+		},
+		b = {
+			"<cmd>Telescope git_branches<cr>",
+			"Checkout branch",
+		},
+		c = {
+			"<cmd>Telescope git_commits<cr>",
+			"Checkout commit",
+		},
 		d = {
 			"<cmd>Gitsigns diffthis HEAD<cr>",
 			"Diff",
@@ -131,7 +203,10 @@ local mappings = {
 
 	l = {
 		name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+		a = {
+			"<cmd>lua vim.lsp.buf.code_action()<cr>",
+			"Code Action",
+		},
 		d = {
 			"<cmd>Telescope lsp_document_diagnostics<cr>",
 			"Document Diagnostics",
@@ -140,9 +215,18 @@ local mappings = {
 			"<cmd>Telescope lsp_workspace_diagnostics<cr>",
 			"Workspace Diagnostics",
 		},
-		f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
-		i = { "<cmd>LspInfo<cr>", "Info" },
-		I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+		f = {
+			"<cmd>lua vim.lsp.buf.formatting()<cr>",
+			"Format",
+		},
+		i = {
+			"<cmd>LspInfo<cr>",
+			"Info",
+		},
+		I = {
+			"<cmd>LspInstallInfo<cr>",
+			"Installer Info",
+		},
 		j = {
 			"<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
 			"Next Diagnostic",
@@ -151,40 +235,63 @@ local mappings = {
 			"<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
 			"Prev Diagnostic",
 		},
-		l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-		q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+		l = {
+			"<cmd>lua vim.lsp.codelens.run()<cr>",
+			"CodeLens Action",
+		},
+		q = {
+			"<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>",
+			"Quickfix",
+		},
+		r = {
+			"<cmd>lua vim.lsp.buf.rename()<cr>",
+			"Rename",
+		},
+		s = {
+			"<cmd>Telescope lsp_document_symbols<cr>",
+			"Document Symbols",
+		},
 		S = {
 			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
 			"Workspace Symbols",
 		},
 	},
+
 	s = {
 		name = "Search",
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-		h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-		M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		R = { "<cmd>Telescope registers<cr>", "Registers" },
-		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-		C = { "<cmd>Telescope commands<cr>", "Commands" },
+		b = {
+			"<cmd>Telescope git_branches<cr>",
+			"Branch",
+		},
+		c = {
+			"<cmd>Telescope colorscheme<cr>",
+			"Colorscheme",
+		},
+		h = {
+			"<cmd>Telescope help_tags<cr>",
+			"Help",
+		},
+		M = {
+			"<cmd>Telescope man_pages<cr>",
+			"Man Page",
+		},
+		r = {
+			"<cmd>Telescope oldfiles<cr>",
+			"Recent File",
+		},
+		R = {
+			"<cmd>Telescope registers<cr>",
+			"Register",
+		},
+		k = {
+			"<cmd>Telescope keymaps<cr>",
+			"Keymap",
+		},
+		C = {
+			"<cmd>Telescope commands<cr>",
+			"Command",
+		},
 	},
 }
 
-local vopts = {
-	mode = "v", -- VISUAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
-local vmappings = {
-	["/"] = { "<ESC><CMD>lua require(\"Comment.api\").toggle_linewise_op(vim.fn.visualmode())<CR>", "Comment" },
-}
-
-which_key.setup(setup)
 which_key.register(mappings, opts)
-which_key.register(vmappings, vopts)
